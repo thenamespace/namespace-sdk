4b12ef068f8786f19d58871cfa01ee3dbce16ebb
nenadmitt <arthy@namespace.tech>
2024-07-26 19:28:49 +0200

    add new l2 contracts

--- src/web3/contract-addresses.ts
+++ src/web3/contract-addresses.ts
@@ -1,11 +1,37 @@
 import { Address, zeroAddress } from "viem";
 import { L2Chain, MainChain } from "../clients";
 
-interface NamespaceL2Contracts {
+export const isLegacyName = (name: string) => {
+  // we have a couple of names that have remained on old contracts
+  // this is a workaround until we migrate 
+  const legacy = ["gotbased.eth","musicaw3.eth"];
+
+  const split = name.split(".");
+  if (split.length === 2) {
+    return legacy.includes(name);
+  }
+
+  if (split.length === 3) {
+    const parent = `${split[1]}.${split[2]}`;
+    return legacy.includes(parent);
+  }
+
+  return false;
+
+}
+
+interface NamespaceL2ContractsLegacy {
   controller: Address;
   factory: Address;
   manager: Address;
   resolver: Address;
+}
+
+interface NamespaceL2Contracts {
+  controller: Address;
+  resolver: Address;
+  registryResolver: Address
+  emitter: Address
 }
 
 interface NamespaceL1Contracts {
@@ -29,7 +55,29 @@ const MainChainAddresses: Record<MainChain, NamespaceL1Contracts> = {
   },
 };
 
-const L2ChainAddresses: Record<L2Chain, NamespaceL2Contracts> = {
+const L2ChainAddresses: Record<L2Chain, NamespaceL2Contracts>  = {
+  base: {
+    controller: "0x27b792d933A905c9BB53b139898d0d79654BE2Ab",
+    resolver: "0xA1f1eE95B22bBfFd5cB72f70573B8c3A762a4Ed8",
+    emitter: "0xd7836B047dD66622b7AAB743E88Dd158dDc23Dc1",
+    registryResolver: "0xBB49Cd94f5cd37bbf19aD9eab5024426A56674e7"
+  },
+  // currently, L2 subnames are only supported for Base chain
+  optimism: {
+    controller: zeroAddress,
+    resolver: zeroAddress,
+    emitter: zeroAddress,
+    registryResolver: zeroAddress
+  },
+  arbitrum: {
+    controller: zeroAddress,
+    resolver: zeroAddress,
+    emitter: zeroAddress,
+    registryResolver: zeroAddress
+  },
+}
+
+const L2ChainAddressesLegacy: Record<L2Chain, NamespaceL2ContractsLegacy> = {
   base: {
     controller: "0x38dB2bA2Fc5A6aD13BA931377F938BBDe831D397",
     factory: "0x994E494506FE8166f2434E52560755D790eF5641",
@@ -68,6 +116,10 @@ export const getMainChainContracts = (chainName: MainChain) => {
 
 export const getL2ChainContracts = (chainName: L2Chain) => {
   return L2ChainAddresses[chainName];
+}
+
+export const getL2ChainContractsLegacy = (chainName: L2Chain) => {
+  return L2ChainAddressesLegacy[chainName];
 };
 
 export const getEnsContracts = (chainName: MainChain) => {
